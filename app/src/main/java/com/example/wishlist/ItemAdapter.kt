b46639c87ed4f060a41f8b730b5525735f9e4dea
matteoSaputo <matteosaputo1201@gmail.com>
2022-09-23 21:06:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/wishlist/ItemAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.wishlist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
+
+class ItemAdapter(private val items: List<Item>): RecyclerView.Adapter<ItemAdapter.ViewHolder>(){
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        // Create member variables for any view that will be set
+        // as you render a row.
+        val itemNameTextView: TextView
+        val priceTextView: TextView
+        val urlTextView: TextView
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each sub-view
+        init {
+            itemNameTextView = itemView.findViewById(R.id.itemName)
+            priceTextView = itemView.findViewById(R.id.price)
+            urlTextView = itemView.findViewById(R.id.url)
+            // Store each of the layout's views into
+            // the public final member variables created above
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val contactView = inflater.inflate(R.layout.item_layout, parent, false)
+        // Return a new holder instance
+        return ViewHolder(contactView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // Get the data model based on position
+        val item = items.get(position)
+        // Set item views based on views and data model
+        holder.itemNameTextView.text = item.name
+        holder.priceTextView.text = item.price
+        holder.urlTextView.text = item.url
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
